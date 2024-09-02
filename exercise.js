e91c9e6dbbe663d387107e942b4a9bae91a7542d
shanemccarthy <shanemcc22@gmail.com>
2024-09-02 07:02:05 -0400

    add fixed code and text answers

---
+++ exercise.js
@@ -0,0 +1,6 @@
+const PI = 3.14; // PI is a constant and cannot be reassigned
+
+let radius = 7; // radius can be reassigned
+radius = 10; // Reassigning radius is allowed
+
+console.log(`PI: ${PI}, Radius: ${radius}`);
